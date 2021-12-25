a3729e7a67474bbc398b0f6dd8e9f349e6831a64
Alexander Sokol <saneea@bk.ru>
2021-12-26 00:39:24 +0300

    refactoring

--- src/app/subjects/lessons.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LessonsPipe } from './lessons.pipe';
-
-describe('LessonsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LessonsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/subjects/subjects.component.ts
+++ src/app/subjects/subjects.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Config} from "../common/config";
 import {Time} from "../common/time";
+import {LessonItem} from "../subject/subject.component";
+import {LocalTime} from "@js-joda/core";
 
 @Component({
   selector: 'app-subjects',
@@ -10,12 +12,38 @@ import {Time} from "../common/time";
 export class SubjectsComponent implements OnInit {
 
   @Input()
-  config?: Config;
+  set config(value: Config) {
+    this._config = value;
+    this.lessons = this.configToLessons(value);
+  }
 
   @Input()
   time?: Time;
 
+  _config?: Config;
+
+  lessons: LessonItem[] = [];
+
   ngOnInit(): void {
+  }
+
+  configToLessons(config: Config): LessonItem[] {
+    let result: LessonItem[] = [];
+
+    let currentStart: LocalTime = LocalTime.of(config.startTime.hours, config.startTime.minutes);
+
+    for (const subject of config.subjects) {
+      currentStart = currentStart.plusMinutes(subject.timeOffset);
+      let currentEnd = currentStart.plusMinutes(subject.duration);
+      result.push({
+        subject: subject,
+        start: currentStart,
+        end: currentEnd
+      });
+      currentStart = currentEnd;
+    }
+
+    return result;
   }
 
 }
