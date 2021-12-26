d99488708ec5ab80c0ffed95e89ca2a975e26083
Alexander Sokol <saneea@bk.ru>
2021-12-26 13:43:51 +0300

    use joda LocalTime

--- src/app/common/time.ts
+++ src/app/common/time.ts
@@ -2,7 +2,3 @@ export interface Time {
   hours: number;
   minutes: number;
 }
-
-export function equalTime(t1: Time, t2: Time): boolean {
-  return t1.hours === t2.hours && t1.minutes === t2.minutes;
-}
