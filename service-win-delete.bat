b6c33d80abddedebfbaa0123049be417bf1afdc2
Alexander Sokol <saneea@bk.ru>
2022-01-08 12:14:18 +0300

    fix Windows service scripts

---
+++ service-win-delete.bat
@@ -0,0 +1,1 @@
+sc delete "Saneea Lessons Schedule"
--- service-win-install.bat
+++ service-win-install.bat
@@ -1,1 +1,1 @@
-sc create "Saneea Lessons Schedule" npx http-server %~dp0dist\lessons-schedule --port 8080
+sc create "Saneea Lessons Schedule" binpath="npx.cmd --yes http-server %~dp0dist\lessons-schedule --port 8080 --silent"
